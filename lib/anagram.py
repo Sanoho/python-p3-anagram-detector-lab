326fe47c4d4aac51f5adec6d8d016bc1dac69536
Timothy <sanohoang@gmail.com>
2023-04-04 15:50:05 -0500

    completed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        self.word_list = word_list
+        matches = []
+        for word_in_list in word_list:
+            if sorted([letter for letter in word_in_list]) == self.word:
+                matches.append(word_in_list)
+        return matches
+        
